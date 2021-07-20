5de487467747b7233aa8567f31ab844de377b6f5
mgtz505 <gutierrez.marcusc@gmail.com>
2021-07-20 17:20:16 -0400

    Exercises 1 & 2

--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hey")
-	fmt.Println("TEST")
+	fmt.Println("Level 1 Exercises")
+
+	//Exercise 1
+	// x := 42
+	// y := "James Bond"
+	// z := true
+	// fmt.Println(x, y, z)
+
+	//Exercise 2
+	var x int
+	var y string
+	var z bool
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", z)
 
 }
