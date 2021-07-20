505a5f3b4ba7ec78338330fbfd875bd2c372d4de
mgtz505 <gutierrez.marcusc@gmail.com>
2021-07-20 17:27:31 -0400

    Exercises Three and Four

--- main.go
+++ main.go
@@ -12,11 +12,26 @@ func main() {
 	// fmt.Println(x, y, z)
 
 	//Exercise 2
-	var x int
-	var y string
-	var z bool
-	fmt.Printf("%T\n", x)
-	fmt.Printf("%T\n", y)
-	fmt.Printf("%T\n", z)
+	// var x int
+	// var y string
+	// var z bool
+	// fmt.Printf("%T\n", x)
+	// fmt.Printf("%T\n", y)
+	// fmt.Printf("%T\n", z)
 
+	// Exercise 3
+	// var x int = 42
+	// var y string = "James Bond"
+	// var z bool = true
+
+	// s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
+	// fmt.Println(s)
+
+	//Exercise 4
+	type magic int
+	var x magic
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
 }
