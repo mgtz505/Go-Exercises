5621d883b1bca08687240960fa76213a4e3e89ba
mgtz505 <gutierrez.marcusc@gmail.com>
2021-07-21 12:04:46 -0400

    Exercise 4

--- main.go
+++ main.go
@@ -167,4 +167,15 @@ func main() {
 	sl4 := sl[2:len(sl)]
 	fmt.Println(sl4)
 
+	//Exercise 4
+
+	x := []int{42, 43, 44, 45, 56, 47, 48, 49, 50, 51}
+	x = append(x, 52)
+	fmt.Println(x)
+	x = append(x, 53, 54, 55)
+	fmt.Println(x)
+	y := []int{56, 57, 58, 59, 60}
+	x = append(x, y...) //Can't forget the ... ellipse
+	fmt.Println(x)
+
 }
